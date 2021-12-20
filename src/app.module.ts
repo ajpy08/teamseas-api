6e577e83828259f8247c9ecf6141efe699250bac
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-12-19 20:26:01 -0600

    first version of graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
+import { GraphQLDateTime } from 'graphql-iso-date';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      resolvers: { DateTime: GraphQLDateTime },
+    }),
+    DonationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
