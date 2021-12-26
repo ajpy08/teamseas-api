db2299a3754caf6c8dff113aab1271b6c3694a33
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-12-26 11:05:31 -0600

    total subscription added

--- src/donations/donations.resolver.ts
+++ src/donations/donations.resolver.ts
@@ -1,18 +1,31 @@
 import { OrderByParams } from './../graphql';
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Subscription } from '@nestjs/graphql';
 import { DonationsService } from './donations.service';
 import { DonationCreateInput } from 'src/@generated/prisma-nestjs-graphql/donation/donation-create.input';
+import { PubSub } from 'graphql-subscriptions';
+
+const pubSub = new PubSub();
 
 @Resolver('Donation')
 export class DonationsResolver {
   constructor(private readonly donationsService: DonationsService) {}
 
   @Mutation('createDonation')
-  create(
+  async create(
     @Args('createDonationInput')
     createDonationInput: DonationCreateInput, // Prisma.DonationCreateInput,
   ) {
-    return this.donationsService.create(createDonationInput);
+    const created = await this.donationsService.create(createDonationInput);
+    const total = await this.donationsService.getTotal();
+
+    pubSub.publish('totalUpdated', { totalUpdated: { total } });
+
+    return created;
+  }
+
+  @Subscription()
+  totalUpdated() {
+    return pubSub.asyncIterator('totalUpdated');
   }
 
   @Query('donations')
